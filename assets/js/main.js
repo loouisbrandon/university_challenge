789489dcbba8243718e261178e99a95e9cfe3f21
Brandon louis <brandonlouisferreira@gmail.com>
2023-01-10 18:51:06 -0300

    0.8

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,62 @@ themeToggler.addEventListener('click', () =>{
 
     themeToggler.querySelector('span').classList.toggle('active')
 })
+
+//EDIT STUDENT
+class Student {
+    constructor() {
+        this.id = 1;
+        this.arrayStudent = [];
+
+    }
+
+    save() {
+        let student = this.readData();
+
+        if(this.inputValidate(student)){
+            this.add(student);
+        }
+
+        console.log(this.arrayStudent);
+    }
+
+    add(student) {
+        this.arrayStudent.push(student);
+        this.id++;
+    }
+
+    readData() {
+        let student = {}
+        
+        student.id = this.id;
+        student.nameStudent = document.getElementById('student').value;
+        student.reg = document.getElementById('Reg').value;
+
+        return student;
+    }
+
+    inputValidate(student) {
+        let message = '';
+
+        if(student.nameStudent == '') {
+            message += '- Please insert the student name \n';
+        }
+
+        if(student.reg == '') {
+            message += '- Please insert the student registration \n';
+        }
+
+        if(message != '') {
+            alert(message);
+            return false
+        }
+
+        return true;
+    }
+
+        remove() {
+   
+        }
+}
+
+var student = new Student();

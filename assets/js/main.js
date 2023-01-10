b02e3f5311a0db1958fca0ac106aa47fb8d8dc60
Brandon louis <brandonlouisferreira@gmail.com>
2023-01-10 20:50:34 -0300

    insert students

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,34 @@ class Student {
             this.add(student);
         }
 
-        console.log(this.arrayStudent);
+        this.listTable();
+        this.cancel();
+    }
+
+    listTable() {
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = '';
+
+        for(let i = 0; i < this.arrayStudent.length; i++){
+            let tr = tbody.insertRow();
+            
+            let td_id = tr.insertCell();
+            let td_nameStudent = tr.insertCell();
+            let td_reg = tr.insertCell();
+            let td_actions = tr.insertCell();
+            
+            td_id.innerText = this.arrayStudent[i].id;
+            td_nameStudent.innerText = this.arrayStudent[i].nameStudent;
+            td_reg.innerText = this.arrayStudent[i].reg;
+            
+
+            let imgDelete = document.createElement('img');
+            imgDelete.src = './assets/images/deleteimage.svg';
+            imgDelete.setAttribute("onClick", "student.remove("+ this.arrayStudent[i].id +")");
+
+            td_actions.appendChild(imgDelete);
+
+        }
     }
 
     add(student) {
@@ -72,8 +99,23 @@ class Student {
         return true;
     }
 
-        remove() {
-   
+        cancel() {
+            document.getElementById('student').value = '';
+            document.getElementById('Reg').value = '';
+        }
+
+        remove(id) {
+            if(confirm('You will delete the student ' + id)) {
+                let tbody = document.getElementById('tbody');
+            
+                for(let i = 0; i < this.arrayStudent.length; i++) {
+                    if(this.arrayStudent[i].id == id){
+                        this.arrayStudent.splice(i, 1);
+                        tbody.deleteRow(i);
+                    }
+                }
+            }
+
         }
 }
 
